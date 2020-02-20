a40e4a05be4cf04d211e1334fc02edf66a14a378
Goshen Jimenez <goshenjimenez@gmail.com>
2020-02-20 16:30:03 +0800

    Data Access class

---
+++ GoshenJimenez.EmployeeQueries/GoshenJimenez.EmployeeQueries.Windows/DAL/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using DbSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using MySql.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoshenJimenez.EmployeeQueries.DAL
+{
+    public class DataAccess : DbContext
+    {
+        public DataAccess() : base("myConnectionString")
+        {
+            
+        }
+
+
+    }
+}
